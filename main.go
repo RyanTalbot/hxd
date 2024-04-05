90828b9d091908ed89cfa08d3fd697f49caed12d
RyanTalbot <ryan-talbot@outlook.com>
2024-04-05 01:24:13 +0100

    Handling file and dumping hex

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("hxd")
+	if len(os.Args) < 1 {
+		os.Exit(1)
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fileBytes := make([]byte, 256)
+	out := make([]byte, 256)
+
+	for {
+		_, err := file.Read(fileBytes)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+
+		buffer := bytes.NewBuffer(fileBytes)
+		err = binary.Read(buffer, binary.BigEndian, &out)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s", hex.Dump(out))
+	}
 }
